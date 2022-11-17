a2f201a85fc61517412762efef4eef3cde94c122
Perkles <scleinaldo85@gmail.com>
2022-11-17 16:15:51 -0300

    Initial commit project

---
+++ src/one/digitalinnovation/gof/strategy/ComportamentoDefensivo.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.gof.strategy;
+
+public class ComportamentoDefensivo implements Comportamento {
+
+    @Override
+    public void mover() {
+        System.out.println("Movendo-se defensivamente...");
+    }
+
+}
+
